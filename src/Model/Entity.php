b3fa9569b350b125934a9820edbdf0bd90182293
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-27 09:08:31 +0100

    [FEAT] Add documentation

--- src/Model/Entity.php
+++ src/Model/Entity.php
@@ -13,10 +13,48 @@ use DateTimeImmutable;
  */
 interface Entity
 {
+    /**
+     * Return the Entity's id.
+     *
+     * @return Uid|null The entity's id
+     */
     public function getId(): ?Uid;
+
+    /**
+     * set the entity's id.
+     *
+     * @param Uid $id The entity's id to set.
+     * @return void
+     */
     public function setId(Uid $id): void;
+
+    /**
+     * Return the entity's content.
+     *
+     * @return string The entity's content.
+     */
     public function getContent(): string;
+
+    /**
+     * Set the entity's content.
+     *
+     * @param string $content The content to set.
+     * @return void
+     */
     public function setContent(string $content): void;
+
+    /**
+     * Return the entity's creation date.
+     *
+     * @return DateTimeImmutable
+     */
     public function getCreatedAt(): DateTimeImmutable;
+
+    /**
+     * Set the entity's creation date.
+     *
+     * @param DateTimeImmutable $date The date to set.
+     * @return void
+     */
     public function setCreatedAt(DateTimeImmutable $date): void;
 }
--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -9,9 +9,28 @@ use App\Model\VO\Uid;
 
 interface Repository
 {
+    /**
+     * Get an entity corresponding to the id.
+     *
+     * @param Uid $id The entity's id.
+     * @return Entity|null The entity corresponding to the provided id.
+     */
     public function getById(Uid $id): ?Entity;
 
+    /**
+     * Persist an entity.
+     *
+     * @param Entity $entity The entity to persist.
+     * @return void
+     */
     public function save(Entity $entity): void;
 
+
+    /**
+     * Delete the entity corresponding to the provided id.
+     *
+     * @param Uid $id The entity's id to delete.
+     * @return void
+     */
     public function delete(Uid $id): void;
 }
