f3aab3bef3a40f813697fc532ecbcae2adecfe86
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-11-27 11:15:26 +0100

    [FEAT][DTO] Update news dto attribute scope to private and add getter/setters to the news DTO

--- src/Model/DTO/NewsDTO.php
+++ src/Model/DTO/NewsDTO.php
@@ -7,17 +7,87 @@ use DateTimeImmutable;
 
 class NewsDTO
 {
-    public ?Uid $id;
-    public string $content;
-    public DateTimeImmutable $createdAt;
+    /**
+     * The id of the DTO
+     *
+     * @var Uid|null
+     */
+    private ?Uid $id;
 
-    public function __construct(
-        ?Uid $id,
-        string $content,
-        DateTimeImmutable $createdAt
-    ) {
+    /**
+     * The DTO content
+     *
+     * @var string
+     */
+    private string $content;
+
+    /**
+     * @var DateTimeImmutable The date of creation of the entity.
+     */
+    private DateTimeImmutable $createdAt;
+
+    public function __construct(?Uid $id, string $content, DateTimeImmutable $createdAt) {
         $this->id = $id;
         $this->content = $content;
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Return the DTO id.
+     *
+     * @return Uid|null The DTO id.
+     */
+    public function getId(): ?Uid {
+        return $this->id;
+    }
+
+    /**
+     * Set the DTO id.
+     *
+     * @param Uid $id The DTO id to set.
+     * @return NewsDTO
+     */
+    public function setId(Uid $id): void {
+        $this->id = $id;
+    }
+
+    /**
+     * Return the DTO content.
+     *
+     * @return string The DTO content
+     */
+    public function getContent(): string {
+        return $this->content;
+    }
+
+    /**
+     * Set the DTO content.
+     *
+     * @param string $content The content to set.
+     * @return NewsDTO
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Get the creation date.
+     *
+     * @return DateTimeImmutable The date of creation of the entity.
+     */
+    public function getCreatedAt(): DateTimeImmutable {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the creation date.
+     *
+     * @param DateTimeImmutable $createdAt The creadate to set.
+     * @return NewsDTO
+     */
+    public function setCreatedAt(DateTimeImmutable $createdAt): void
+    {
         $this->createdAt = $createdAt;
     }
 }
