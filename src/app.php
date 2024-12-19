eac8ff7fef1a68e08ded7f35db6d677154b658d3
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-12-19 14:11:12 +0100

    [FEAT][INSCRIPTION] Add entry point app.php implementation

--- src/app.php
+++ src/app.php
@@ -3,3 +3,34 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use APP/Command/AddUserCommand;
+use APP/Command/UpdateUserCommand;
+use APP/Command/DeleteUserCommand;
+
+$args = getopt('', ['action:', 'data:']);
+
+if (isset($args['action'])) {
+    echo "Error: No action specified.\n";
+    exit(1);
+}
+
+$action = $args['action'];
+$data = isset($args['data']) ? json_decode($args['data']) : null;
+
+switch ($action) {
+    case 'add':
+        $command = new AddUserCommand();
+        $command->execute($data);
+        break;
+    case 'update':
+        $command = new UpdateUserCommand();
+        $command->execute($data);
+        break;
+    case 'delete':
+        $command = new DeleteUserCommand();
+        $command->execute($data);
+        break;
+    default:
+        echo "Error: Invalid action.\n";
+}
